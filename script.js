af406b2b7e3b83f27f8e2bcf32df4cac910ec602
Cassandre <cassandrelambertd@gmail.com>
2025-04-05 23:38:36 +0200

    Partie 5

--- script.js
+++ script.js
@@ -86,3 +86,43 @@
         document.getElementById('partie-4').innerHTML = all_months_html;
     }
     generateAllMonths();
+    // Partie 5
+    function generateCurrentMonthWithCurrentDay() {
+        const month_names = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+        const month_max_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        let today = new Date();
+        let current_month = today.getMonth();
+        let current_day = today.getDate();
+        let month_name = month_names[current_month];
+        let max_days = month_max_days[current_month];
+
+        let table = '<h3>' + month_name + '</h3>';
+        table += print_table_month_with_today(max_days, current_day);
+        
+        document.getElementById('partie-5').innerHTML = table;
+    }
+
+    function print_table_month_with_today(max_days, today) {
+        let current_day = 1;
+        let table = '<table>';
+        for (let i = 0; i < Math.ceil(max_days / 7); i++) {
+            table += '<tr>';
+            for (let j = 0; j < 7; j++) {
+                if (current_day <= max_days) {
+                    table += '<td' + (current_day === today ? ' class="today"' : '') + '>' + current_day + '</td>';
+                    current_day++;
+                }
+            }
+            table += '</tr>';
+        }
+        table += '</table>';
+        return table;
+    }
+    generateCurrentMonthWithCurrentDay();
+
+    // Debug mode
+    if (debug_mode) {
+        let debug_html = document.body.innerHTML;
+        document.body.innerHTML = '<textarea>' + debug_html + '</textarea>';
+    }
+});
